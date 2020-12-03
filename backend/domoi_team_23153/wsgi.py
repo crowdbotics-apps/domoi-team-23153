c753bb969fa08c258ecc5000f53590821908bc48
Crowdbotics <team@crowdbotics.com>
2020-12-03 05:20:38 +0000

    Update models

--- backend/domoi_team_23153/wsgi.py
+++ backend/domoi_team_23153/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'domoi_team_23153.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "domoi_team_23153.settings")
 
 application = get_wsgi_application()
